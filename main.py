3ddac07b389c95318ff686acd201ec85715d5566
jais_vicky <ce21btech11045@iith.ac.in>
2025-03-17 23:09:29 +0530

    fix: added gender x other parameter interaction

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ print("6 - Effect of Age x Gender on ADAS (Both Original & Perceived Data)")
 print("7 - Effect of Age x Crash Experience on ADAS (Both Original & Perceived Data)")
 print("8 - Effect of Age x Driver Education on ADAS (Both Original & Perceived Data)")
 print("9 - Effect of Age x Drving Experience on ADAS (Both Original & Perceived Data)")
+print("10 - Effect of Gender x Crash Experience on ADAS (Both Original & Perceived Data)")
+print("11 - Effect of Gender x Driver Education on ADAS (Both Original & Perceived Data)")
+print("12 - Effect of Gender x Driving Experience on ADAS (Both Original & Perceived Data)")
 os.makedirs("plot", exist_ok=True)
 
 choice = input("\nEnter your choice: ")
@@ -32,5 +35,11 @@ elif choice == "8":
     os.system("python3 age_education_effect_analysis.py")
 elif choice == "9":
     os.system("python3 age_driving_effect_analysis.py")
+elif choice == "10":
+    os.system("python3 gender_crash_effect_analysis.py")
+elif choice == "11":
+    os.system("python3 gender_education_effect_analysis.py")
+elif choice == "12":
+    os.system("python3 gender_driving_effect_analysis.py")
 else:
     print("Invalid choice! Please run the script again.")
